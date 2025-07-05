60d265eeaf946522cf4b38d86bb58ab4abf244d7
eduardo <198397564+esb-11@users.noreply.github.com>
2025-07-05 14:54:33 -0300

    feat: add webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { watchFile } = require("fs");
 
 module.exports = {
   mode: "development",
