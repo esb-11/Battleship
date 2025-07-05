2f29909b3f3c4d892b55b3b76d02a16f06edb53b
eduardo <198397564+esb-11@users.noreply.github.com>
2025-07-05 09:08:41 -0300

    refactor: make Player class a module

--- src/scripts/Player.js
+++ src/scripts/Player.js
@@ -1,9 +1,28 @@
+import Ship from "./Ship.js";
 import Gameboard from "./Gameboard.js";
 
-class Player {
-  constructor() {
-    this.board = new Gameboard();
+const Player = (() => {
+  const STARTING_FLEET = ["carrier", "battleship", "destroyer", "submarine", "patrol boat"];
+
+  function createPlayer(name = "Player") {
+    const board = Gameboard.createBoard();
+
+    fillBoard(board);
+
+    return { name, board };
   }
-}
+
+  function createCPU() {}
+
+  function fillBoard(board) {
+    let index = 0;
+    STARTING_FLEET.forEach((ship) => {
+      board.placeX(new Ship(ship), [index, 0]);
+      index += 2;
+    });
+  }
+
+  return { createPlayer, createCPU };
+})();
 
 export default Player;
--- src/scripts/tests/Player.test.js
+++ src/scripts/tests/Player.test.js
@@ -1,3 +1,13 @@
 import Player from "../Player.js";
 
-test();
+test("Correctly places the starting fleet", () => {
+  const player = Player.createPlayer();
+  let board = JSON.parse(player.board.getBoard()); 
+
+  expect(board[0][0]).toBeTruthy();
+  expect(board[1][0]).toBeTruthy();
+  expect(board[2][0]).toBeTruthy();
+  expect(board[3][0]).toBeTruthy();
+  expect(board[4][0]).toBeTruthy();
+  expect(board[5][0]).toBeFalsy();
+});
